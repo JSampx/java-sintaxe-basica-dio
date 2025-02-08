56e169d7c2e2a7ad4ec0f5c230ec8ec29291c12e
João Sampaio <joao.sampaio.andrade@gmail.com>
2025-02-08 17:36:29 -0300

    Arquivos do exercício

---
+++ ContaBanco/src/Conta.java
@@ -0,0 +1,48 @@
+public class Conta {
+    private int numeroConta;
+    private String numeroAgencia;
+    private String nomeCliente;
+    private Double saldo;
+
+    public Conta() {
+    }
+
+    public Conta(int numeroConta, String numeroAgencia, String nomeCliente, Double saldo) {
+        this.numeroConta = numeroConta;
+        this.numeroAgencia = numeroAgencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(String numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
